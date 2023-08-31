a12293b02962c56f733718943e833691aa94c721
1808charitha <charithakamireddy18@gmail.com>
2023-08-31 18:29:08 +0530

    first commit

---
+++ src/Even.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Even {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println(even_odd(number));
+    }
+
+    static String even_odd(int number) {
+        if (number % 2 == 0) {
+            return "The Number is even";
+        } else {
+            return "The Number is odd";
+        }
+    }
+}
